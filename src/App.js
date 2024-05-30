d1acb0918096aa41e3cb0ec20382f47aed6e849b
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-30 18:05:08 +0100

    Continue here

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog!";
-  const likes = 50;
-  const person = { name: "yoshi", age: 30 };
-  const link = "https://google.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
-        <p>Liked {likes} times</p>
-
-        {/* <p>{person}</p> */}
-        <p>{" hello ninjas "}</p>
-        <p>"Array: {[1, 2, 3, 4, 5]}"</p>
-        <p>"Random number: {Math.random() * 10}"</p>
-
-        <p>
-          Dynamic values link:
-          <a href="link">Google</a>
-        </p>
+        <Home />
       </div>
     </div>
   );
 }
 
 export default App;
+
+// Video: https://www.youtube.com/watch?v=NbTrGcz4DW8&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=6

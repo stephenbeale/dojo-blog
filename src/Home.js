4e286b9602b252897c2586231594cb10c1982d90
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-06-03 12:24:54 +0100

    WIP - outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,13 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log("hello, ninjas", e);
-  };
+import { useState } from "react";
 
-  const handleClickAgain = (name, e) => {
-    console.log("hello " + name, e.target);
-  };
-
+const Home = () => {  
+const [blogs, setBlogs] = useState([
+  {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+  {title: 'Hello World', body: 'lorem ipsum...', author: 'luigi', id: 2},
+  {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'yoshi', id: 3}
+]);
   return (
-    <div className="home">
-      <h2>Home Page</h2>
-      <button onClick={handleClick}>Click me!</button>
-      <button
-        onClick={(e) => {
-          handleClickAgain("mario", e);
-        }}
-      >
-        Click me again{" "}
-      </button>
+    <div className="home">      
     </div>
   );
 };

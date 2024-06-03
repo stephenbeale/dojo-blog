1f6bbda3df29ceaa646748c100b3c1351637cbc1
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-06-03 11:41:49 +0100

    WIP - anonymous functions

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("hello, ninjas");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("hello " + name);
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me!</button>
+      <button
+        onClick={() => {
+          handleClickAgain("mario");
+        }}
+      >
+        Click me again{" "}
+      </button>
     </div>
   );
 };

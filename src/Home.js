79f4be0ef4e4c79b80559ccdd73e50679b6f78bb
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-06-06 14:26:57 +0100

    Dependency array - empty as a test

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./bloglist";
 
 const Home = () => {
@@ -18,6 +18,13 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  //Will run every time there is a re-render
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(blogs);
+  }, []);
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />

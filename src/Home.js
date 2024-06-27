4e0d3d4c69d55ebbe5e0aeb20a178cdee178198f
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-06-27 12:35:05 +0100

    Getting back after hols

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,7 @@
-import { useState, useEffect } from "react";
-import BlogList from "./bloglist";
-
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "luigi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "yoshi",
-      id: 3,
-    },
-  ]);
-
-  const [name, setName] = useState("mario");
-
-  //Remove any blogs from the array that DO NOT match the given id, i.e. only leaving blogs with matching id in to be deleted.
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  //Will run every time there is a re-render
-  useEffect(() => {
-    console.log("use effect ran");
-    console.log(name);
-  }, [name]);
-
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName("luigi")}>Change Name</button>
-      <p>{name}</p>
+      <h2>Home</h2>
     </div>
   );
 };

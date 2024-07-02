99f3d11fac8651135e40d4685fda7734fb0b07c6
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-07-02 11:27:10 +0100

    Throw error in with catch block

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,9 @@ const Home = () => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {

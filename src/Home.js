ba4dee0eee148a95ac2a4820f62c5670ec26341e
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-07-02 10:26:02 +0100

    Tutorial 17 - fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import BlogList from "./bloglist";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  //Will run every time there is a re-render
+  useEffect(() => {
+    //Then - fires a function once we have the data back
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      {/* Blogs and works through conditional templating - 'logical and' from JS evaluates left side first, but if null, then evaluates right */}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };

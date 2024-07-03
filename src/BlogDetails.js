8fb08514b0690847bc759f2ed13278d5c7d6739e
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-07-03 15:40:58 +0100

    Outputting ID on DOM

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
+  const { id } = useParams();
   return (
     <div className="blog-details">
-      <h2>Blog Details</h2>
+      <h2>Blog Details {id} </h2>
     </div>
   );
 };

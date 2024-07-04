c4392b9a235e90bdec51a2f9d9a1f8dbf2b07df6
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-07-04 16:48:15 +0100

    Delete blog now re-routes to home page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -7,7 +7,17 @@ const BlogDetails = () => {
     data: blog,
     error,
     isPending,
-  } = useFetch(`http://localhost:8000/blogs/${id}`);
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -17,6 +27,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>

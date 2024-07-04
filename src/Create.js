995b92df225a4ace8aa06e9d5cb35af3967b40b6
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-07-04 15:08:12 +0100

    Outputting body and title as we type.

--- src/Create.js
+++ src/Create.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 
 const Create = () => {
-  const [title, setTitle] = useState("hello");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
       <form>
         <label>Blog title:</label>
+        {/* onChange here means that we update the title as we type, using state.
+        Target is the input field. */}
         <input
           type="text"
           required
@@ -15,13 +18,19 @@ const Create = () => {
           onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog body:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={title}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog author:</label>
         <select>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
         <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
       </form>
     </div>
   );

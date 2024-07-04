107ec1a7bcce1c5b5e4cfc90546e04bc64bf9225
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-07-04 15:04:18 +0100

    Tracking value in state from form input

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("hello");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
       <form>
         <label>Blog title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog body:</label>
         <textarea required></textarea>
         <label>Blog author:</label>
